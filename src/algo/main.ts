275c7451db1211f67d4bcb2229c626f44e87db43
darrickyong <darrickyong@gmail.com>
2023-06-17 21:04:56 -0700

    Add 3 accessory calculation

--- src/algo/main.ts
+++ src/algo/main.ts
@@ -81,7 +81,7 @@ enum RequiredNodesEnum {
   FiveThreeTwo = 85,
 }
 interface RequiredNodes {
-  total: RequiredNodesEnum;
+  total: number;
   nodes: RequiredEngravings[];
 }
 
@@ -113,13 +113,122 @@ const findEngravingAndDecrement = (
   return remainingNodes;
 };
 
+const createListofAccessories = (requiredEngravings: (Combat | Class)[]) => {
+  const listOfAccessories: Accessory[] = [];
+  for (let i = 0; i < requiredEngravings.length; i++) {
+    for (let j = 0; j < requiredEngravings.length; j++) {
+      if (i !== j) {
+        const primaryEngraving = requiredEngravings[i];
+        const secondaryEngraving = requiredEngravings[j];
+        listOfAccessories.push({
+          eng1: { name: primaryEngraving, value: 3 },
+          eng2: { name: secondaryEngraving, value: 3 },
+        });
+        // listOfAccessories.push({
+        //   eng1: { name: primaryEngraving, value: 3 },
+        //   eng2: { name: secondaryEngraving, value: 3 },
+        // });
+        // listOfAccessories.push({
+        //   eng1: { name: primaryEngraving, value: 3 },
+        //   eng2: { name: secondaryEngraving, value: 3 },
+        // });
+        listOfAccessories.push({
+          eng1: { name: primaryEngraving, value: 4 },
+          eng2: { name: secondaryEngraving, value: 3 },
+        });
+        // listOfAccessories.push({
+        //   eng1: { name: primaryEngraving, value: 4 },
+        //   eng2: { name: secondaryEngraving, value: 3 },
+        // });
+        // listOfAccessories.push({
+        //   eng1: { name: primaryEngraving, value: 4 },
+        //   eng2: { name: secondaryEngraving, value: 3 },
+        // });
+        listOfAccessories.push({
+          eng1: { name: primaryEngraving, value: 5 },
+          eng2: { name: secondaryEngraving, value: 3 },
+        });
+        // listOfAccessories.push({
+        //   eng1: { name: primaryEngraving, value: 5 },
+        //   eng2: { name: secondaryEngraving, value: 3 },
+        // });
+        // listOfAccessories.push({
+        //   eng1: { name: primaryEngraving, value: 5 },
+        //   eng2: { name: secondaryEngraving, value: 3 },
+        // });
+        listOfAccessories.push({
+          eng1: { name: primaryEngraving, value: 6 },
+          eng2: { name: secondaryEngraving, value: 3 },
+        });
+        // listOfAccessories.push({
+        //   eng1: { name: primaryEngraving, value: 6 },
+        //   eng2: { name: secondaryEngraving, value: 3 },
+        // });
+        // listOfAccessories.push({
+        //   eng1: { name: primaryEngraving, value: 6 },
+        //   eng2: { name: secondaryEngraving, value: 3 },
+        // });
+      }
+    }
+  }
+  return listOfAccessories;
+};
+
+const isEngravingEnough = (total: number, nodes: RequiredEngravings[], listOfAccessories: Accessory[]) => {
+  // console.log('ENOUGH?', nodes);
+  const res = { total, nodes };
+  listOfAccessories.forEach((acc) => {
+    findEngravingAndDecrement(res, acc.eng1);
+    findEngravingAndDecrement(res, acc.eng2);
+  });
+  if (res.total === 0) {
+    console.log('ENOUGH', listOfAccessories);
+    return true;
+  }
+  return false;
+};
+
 interface CalculateAccessories {
   total: number;
   remainingAcc: number;
-  nodes: EngravingAccessory[];
+  nodes: RequiredEngravings[];
 }
+
 const calculateAccessories = ({ total, nodes, remainingAcc }: CalculateAccessories) => {
+  // console.log('INITIAL NODES', nodes);
   const requiredEngravings = nodes.map((node) => node.name);
+  const res: Accessory[][] = [];
+  const listOfAccessories = createListofAccessories(requiredEngravings);
+  // console.log('LIST OF ACCESSORIES', listOfAccessories);
+  let iterationOfAcc: Accessory[] = [];
+
+  if (remainingAcc > 0) {
+    for (let i = 0; i < listOfAccessories.length; i++) {
+      iterationOfAcc[0] = listOfAccessories[i];
+      if (remainingAcc > 1) {
+        for (let j = 0; j < listOfAccessories.length; j++) {
+          iterationOfAcc[1] = listOfAccessories[j];
+          if (remainingAcc > 2) {
+            for (let k = 0; k < listOfAccessories.length; k++) {
+              iterationOfAcc[2] = listOfAccessories[k];
+              if (isEngravingEnough(total, structuredClone(nodes), iterationOfAcc)) res.push(iterationOfAcc);
+            }
+          } else {
+            if (isEngravingEnough(total, structuredClone(nodes), iterationOfAcc)) res.push(iterationOfAcc);
+          }
+        }
+      } else {
+        if (isEngravingEnough(total, structuredClone(nodes), iterationOfAcc)) res.push(iterationOfAcc);
+      }
+    }
+  } else return [];
+
+  // console.log('-------------');
+  // res.forEach((acc) => {
+  //   console.log(acc);
+  // });
+
+  return res.length;
 };
 
 export const engravingAlgo = ({ books, existingAcc, requiredNodes, stone }: EngravingReq) => {
@@ -141,11 +250,11 @@ export const engravingAlgo = ({ books, existingAcc, requiredNodes, stone }: Engr
 
   // Determine how many accessories we will
   const remainingAcc = 5 - existingAcc.length;
-  console.log('REMAINING ACCESSORIES: ', remainingAcc);
+  // console.log('REMAINING ACCESSORIES: ', remainingAcc);
 
   // Determine if ancient accessories are needed
   const needAncients = res.total / 8 > remainingAcc;
-  console.log('NEEDS ANCIENTS?', needAncients);
+  // console.log('NEEDS ANCIENTS?', needAncients);
 
   // Order nodes by value, highest first
   res.nodes = orderBy(res.nodes, ['value'], ['desc']);
@@ -153,30 +262,45 @@ export const engravingAlgo = ({ books, existingAcc, requiredNodes, stone }: Engr
   return res;
 };
 
-console.log(
-  engravingAlgo({
-    books: DEMO_BOOKS as [EngravingBook, EngravingBook],
-    requiredNodes: DEMO_REQUIRED_NODES,
-    stone: DEMO_STONE,
-    existingAcc: [
-      DEMO_NECKLACE,
-      DEMO_EARRING1,
-      DEMO_EARRING2,
-      // DEMO_RING1,
-      // DEMO_RING2,
-    ],
-  })
-);
+const testing = engravingAlgo({
+  books: DEMO_BOOKS as [EngravingBook, EngravingBook],
+  requiredNodes: DEMO_REQUIRED_NODES,
+  stone: DEMO_STONE,
+  existingAcc: [
+    // DEMO_NECKLACE,
+    // DEMO_EARRING1,
+    // DEMO_EARRING2,
+    DEMO_RING1,
+    DEMO_RING2,
+  ],
+});
+
+console.log(testing);
+
+// {
+//   total: 26,
+//   nodes: [
+//     { name: 'Demonic Impulse', value: 6 },
+//     { name: 'Keen Blunt Weapon', value: 6 },
+//     { name: 'Adrenaline', value: 6 },
+//     { name: 'Hit Master', value: 5 },
+//     { name: 'Grudge', value: 3 }
+//   ]
+// }
+
+// {
+//   total: 24,
+//   nodes: [
+//     { name: 'Hit Master', value: 15 },
+//     { name: 'Keen Blunt Weapon', value: 6 },
+//     { name: 'Adrenaline', value: 3 }
+//   ]
+// }
 
 console.log(
   calculateAccessories({
-    total: 18,
-    remainingAcc: 2,
-    nodes: [
-      { name: Class['Demonic Impulse'], value: 6 },
-      { name: Combat.Adrenaline, value: 6 },
-      { name: Combat.Grudge, value: 3 },
-      { name: Combat['Keen Blunt Weapon'], value: 3 },
-    ],
+    total: testing.total,
+    remainingAcc: 3,
+    nodes: testing.nodes,
   })
 );
